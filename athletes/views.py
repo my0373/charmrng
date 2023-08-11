45ab2605de7a6cade62dc9597d02f17eefc80cb5
Matt York <my0373@gmail.com>
2023-08-11 19:47:01 +0100

    Fixed typos and added django debug toolbar.

--- athletes/views.py
+++ athletes/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def say_hello(request):
-    x = 1
-    y = 2
     return render(request, 'list.html')
     
--- charmrng/urls.py
+++ charmrng/urls.py
@@ -21,4 +21,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('athletes/',include('athletes.urls')),
+    path("__debug__/", include("debug_toolbar.urls")),
+    
+    
 ]
