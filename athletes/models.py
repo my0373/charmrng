c5c55b2d77e5a49eabf5fd79f884e2d54ac5fd8e
Matt York <my0373@gmail.com>
2023-08-25 22:16:46 +0100

    Added a simple athlete model

--- athletes/models.py
+++ athletes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Athlete(models.Model):
+    forename = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    dob = models.DateField()
+    last_update = models.DateTimeField(auto_now=True)
+    photo_permission = models.BooleanField()
+    disability_class = models.CharField(max_length=3)
